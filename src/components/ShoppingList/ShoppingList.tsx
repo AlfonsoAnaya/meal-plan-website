12a6af4276e8dcf151154be1d26e057a4287853f
Alfonso Anaya <alfonso.ws@gmail.com>
2024-02-13 22:28:58 +0100

    fixed bug missing dependency useeffect

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -51,115 +51,115 @@ function ShoppingList() {
                 setListIngredients((prevState) => [...prevState, ingredient])
             })
         });
-    }, [selectedRecipes]);
+    }, [selectedRecipes, weeklyRecipes]);
 
-// UPDATE THE STATE HOLDING THE INGREDIENTS BY CATEGORY EACH TIME THE LIST OF INGREDIENTS CHANGE
-useEffect(() => {
-    setIngredientsState(initialIngredientsState);
-    listIngredients.forEach((ingredient) => {
-        updateIngredientsState(ingredient.category, ingredient)
-    })
-}, [listIngredients])
+    // UPDATE THE STATE HOLDING THE INGREDIENTS BY CATEGORY EACH TIME THE LIST OF INGREDIENTS CHANGE
+    useEffect(() => {
+        setIngredientsState(initialIngredientsState);
+        listIngredients.forEach((ingredient) => {
+            updateIngredientsState(ingredient.category, ingredient)
+        })
+    }, [listIngredients])
 
-// TOGGLE STRIKETHROUGH
-const toggleStrikethrough = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-    e.currentTarget.classList.toggle("line-through");
-    e.currentTarget.classList.toggle("text-gray-500");
-    e.currentTarget.classList.toggle("selected-ingredient")
-}
-
-// ADD AND DELETE RECIPES FROM SELECTION
-function handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>, day: string) {
-    const dayNum = parseInt(day)-1;
-
-    if (!selectedRecipes.includes(dayNum)) {
-        setSelectedRecipes((prevState) => [...prevState, dayNum]);
-    } else {
-        let newState = [...selectedRecipes];
-        newState.splice(newState.indexOf(dayNum));
-        setSelectedRecipes(newState);
+    // TOGGLE STRIKETHROUGH
+    const toggleStrikethrough = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        e.currentTarget.classList.toggle("line-through");
+        e.currentTarget.classList.toggle("text-gray-500");
+        e.currentTarget.classList.toggle("selected-ingredient")
     }
 
-    e.currentTarget.classList.toggle("selected-day")
-};
+    // ADD AND DELETE RECIPES FROM SELECTION
+    function handleClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>, day: string) {
+        const dayNum = parseInt(day) - 1;
+
+        if (!selectedRecipes.includes(dayNum)) {
+            setSelectedRecipes((prevState) => [...prevState, dayNum]);
+        } else {
+            let newState = [...selectedRecipes];
+            newState.splice(newState.indexOf(dayNum));
+            setSelectedRecipes(newState);
+        }
+
+        e.currentTarget.classList.toggle("selected-day")
+    };
 
 
-return (
-    <section className="shopping-list w-[100%] flex flex-col items-center p-4
+    return (
+        <section className="shopping-list w-[100%] flex flex-col items-center p-4
         mt-0 md:mt-12">
-        <div className="max-w-[1100px] w-[100%] flex gap-8"
-        >
-            <div className="w-[100%] md:w-[30%] flex flex-col gap-[.5em]">
-                {weeklyRecipes.map((recipe, i) => {
-                    return (
-                        <div key={`Day ${i}`} id={`Day ${i + 1}`}>
-                            <div
-                                className={`border-b-[1px] border-b-gray-500 flex flex-col`}
-                            >
+            <div className="max-w-[1100px] w-[100%] flex gap-8"
+            >
+                <div className="w-[100%] md:w-[30%] flex flex-col gap-[.5em]">
+                    {weeklyRecipes.map((recipe, i) => {
+                        return (
+                            <div key={`Day ${i}`} id={`Day ${i + 1}`}>
                                 <div
-                                    className="hover:cursor-pointer"
-                                    onClick={(e) => handleClick(e, weekDays[i])}
+                                    className={`border-b-[1px] border-b-gray-500 flex flex-col`}
                                 >
-                                    <ShoppingListCard
-                                        recipe={recipe}
-                                        weekDay={weekDays[i]}
-                                    />
+                                    <div
+                                        className="hover:cursor-pointer"
+                                        onClick={(e) => handleClick(e, weekDays[i])}
+                                    >
+                                        <ShoppingListCard
+                                            recipe={recipe}
+                                            weekDay={weekDays[i]}
+                                        />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>
-            <div className="w-[100%] md:w-[60%]">
-                <h3 className="font-[600] mb-[.5rem] underline
+                        )
+                    })}
+                </div>
+                <div className="w-[100%] md:w-[60%]">
+                    <h3 className="font-[600] mb-[.5rem] underline
                     text-[1.1rem] md:text-[1.5rem] 
                     mt-2 md:mt-0">Ingredientes</h3>
-                <ul>
+                    <ul>
 
-                    {/* MAP THROUGH THE KEYS OF THE OBEJCT HOLDING THE CATEGORIZED INGREDIENTS */}
-                    {Object.keys(ingredientsState).map((key) => {
-                        return (
+                        {/* MAP THROUGH THE KEYS OF THE OBEJCT HOLDING THE CATEGORIZED INGREDIENTS */}
+                        {Object.keys(ingredientsState).map((key) => {
+                            return (
 
-                            // RENDER A CATEGORY TITLE ONLY WHEN IT CONTAINS AT LEAST ONE ELEMENT
-                            ingredientsState[key].length > 0 ?
-                                <div key={key} className="font-sans">
-                                    <h3 className="text-[1.1rem] md:text-[1.25rem] font-[600]">{key}</h3>
-                                    <ul className="mb-[1em]
+                                // RENDER A CATEGORY TITLE ONLY WHEN IT CONTAINS AT LEAST ONE ELEMENT
+                                ingredientsState[key].length > 0 ?
+                                    <div key={key} className="font-sans">
+                                        <h3 className="text-[1.1rem] md:text-[1.25rem] font-[600]">{key}</h3>
+                                        <ul className="mb-[1em]
                                         text-[1rem] md:text-[1.1rem]
                                         font-[300] md:font-[400]">
 
-                                        {/* RENDER THE LIST OF INGREDIENTS IN EACH KEY  */}
-                                        {ingredientsState[key].map((ingredient: Ingredient, i: number) => {
-                                            return (
-                                                <li key={ingredient.name.singular + i}
-                                                    className="shopping-list-item mb-[.5em] flex gap-2 items-center"
-                                                    onClick={toggleStrikethrough}>
-                                                    <span className="checkbox">
-                                                        <span className="checkmark"></span>
-                                                    </span>
-                                                    {ingredient.unit === "unidad" ?
-                                                        (ingredient.quantity > 1 ?
-                                                            <span>{`${ingredient.quantity} ${ingredient.name.plural} `}</span>
-                                                            : <span>{`${ingredient.quantity} ${ingredient.name.singular} `}</span>
-                                                        )
-                                                        : <span>{`${ingredient.quantity} ${ingredient.unit} de ${ingredient.name.singular} `}</span>
-                                                    }
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-                                : ''
-                        )
-                    })}
+                                            {/* RENDER THE LIST OF INGREDIENTS IN EACH KEY  */}
+                                            {ingredientsState[key].map((ingredient: Ingredient, i: number) => {
+                                                return (
+                                                    <li key={ingredient.name.singular + i}
+                                                        className="shopping-list-item mb-[.5em] flex gap-2 items-center"
+                                                        onClick={toggleStrikethrough}>
+                                                        <span className="checkbox">
+                                                            <span className="checkmark"></span>
+                                                        </span>
+                                                        {ingredient.unit === "unidad" ?
+                                                            (ingredient.quantity > 1 ?
+                                                                <span>{`${ingredient.quantity} ${ingredient.name.plural} `}</span>
+                                                                : <span>{`${ingredient.quantity} ${ingredient.name.singular} `}</span>
+                                                            )
+                                                            : <span>{`${ingredient.quantity} ${ingredient.unit} de ${ingredient.name.singular} `}</span>
+                                                        }
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                                    : ''
+                            )
+                        })}
 
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
 
 
-    </section>
-)
+        </section>
+    )
 }
 
 export default ShoppingList;
