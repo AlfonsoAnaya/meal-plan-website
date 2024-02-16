5bd22188e46e10245eae32828778bae02e20be85
Alfonso Anaya <alfonso.ws@gmail.com>
2024-02-16 22:00:49 +0100

    new foto better caorusel

--- src/components/Shared/RecipeCarousel.tsx
+++ src/components/Shared/RecipeCarousel.tsx
@@ -7,7 +7,7 @@ function RecipeCarousel() {
   return (
     <section className="todays-recipe-section flex flex-col items-center bg-primary">
       <h3 className="text-[2rem] text-white font-[600] pt-10">El plan de esta semana</h3>
-      <div className="recipe-grid flex flex-row overflow-x-scroll justify-start gap-[1.75rem] w-[100%] mx-[2rem] py-[2rem]">
+      <div className="recipe-grid flex flex-row justify-center gap-[1.75rem] w-[90%] mx-[2rem] py-[2rem]">
         {recipes.map((recipe, i) => {
           return (
           <Link 
